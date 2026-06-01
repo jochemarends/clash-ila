a8e2d31910817f12c7e5a709127fd5e895a8bdce
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2026-06-01 12:48:17 +0200

    Add example usage to documention of `PolarPrompt`

--- ila-cli/src/ui/polarinput.rs
+++ ila-cli/src/ui/polarinput.rs
@@ -22,6 +22,34 @@ pub enum PolarPromptEvent {
 /// Consists of a message with a horizontal list presenting the two options.
 ///
 /// The left and right options correspond to the positive and negative answers, respectively.
+///
+/// # Examples
+///
+/// ```
+///  use ratatui::{prelude::*, widgets::{Block, Paragraph, WidgetRef}};
+///  use crate::ui::polarinput::PolarPrompt;
+//
+///  let block = Block::bordered();
+//
+///  let prompt = PolarPrompt::new(
+///      Paragraph::new("Make a coffee?").centered(),
+///      ("Confirm".to_string(), "Cancel".to_string()),
+///  );
+//
+///  let width = 40u16;
+///  let area = Rect {x: 0, y: 0, width, height: (prompt.line_count(width) + 2) as u16};
+///  let mut buf = Buffer::empty(area);
+//
+///  block.render_ref(area, &mut buf);
+///  prompt.render_ref(block.inner(area), &mut buf);
+//
+///  // Renders
+///  // ┌──────────────────────────────────────┐
+///  // │            Make a coffee?            │
+///  // │                                      │
+///  // │     [Confirm]           Cancel       │
+///  // └──────────────────────────────────────┘
+/// ```
 #[derive(Debug, Clone)]
 pub struct PolarPrompt<'a> {
     message: Paragraph<'a>,
